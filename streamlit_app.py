738e311c63564075d0b41af914e9d79aa13b24dc
ElenaPopescu88 <102527974+ElenaPopescu88@users.noreply.github.com>
2023-01-30 11:51:46 +0200

    Update streamlit_app.py
    change order

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,6 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.title('My Parents New Healthy Diner')
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
 
 
 
@@ -26,3 +23,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
