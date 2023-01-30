b3869479964158ca2db20771ac2566d58b1530e0
ElenaPopescu88 <102527974+ElenaPopescu88@users.noreply.github.com>
2023-01-30 14:18:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.title('My Parents New Healthy Diner')
 
+import snowflake.connector
 
 
 
@@ -36,6 +37,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
-import snowflake.connector
+
 
 
